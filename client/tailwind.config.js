0051778f0aa780b9dddbd02a64edf03387e3297d
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-01-31 14:04:14 +0100

    config(FE): modified tailwinf config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,41 +1,40 @@
-const colors = require("tailwindcss/colors");
-
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      blue: {
-        light: "#879be5",
-        DEFAULT: "#4766D7",
-        dark: "#2848B8",
-      },
-      green: {
-        light: "#90E0CC",
-        DEFAULT: "#a0e4d3",
-        dark: "#50CEAE",
-      },
-      gray: {
-        light: "#A9ACB2",
-        DEFAULT: "#60646c",
-      },
-      white: {
-        dark: "#EBEBEB",
-        DEFAULT: "#FFFFFF",
+    extend: {
+      colors: {
+        blue: {
+          light: "#879be5",
+          DEFAULT: "#4766D7",
+          dark: "#2848B8",
+        },
+        green: {
+          light: "#90E0CC",
+          DEFAULT: "#a0e4d3",
+          dark: "#50CEAE",
+        },
+        gray: {
+          light: "#A9ACB2",
+          DEFAULT: "#60646c",
+        },
+        white: {
+          dark: "#EBEBEB",
+          DEFAULT: "#FFFFFF",
+          cream: "#F5F5F5",
+          ghost: "#EFF1FB",
+        },
       },
     },
-    // extend: {
-    //   color: {
-    //     "light-blue": "#4766D7",
-    //   },
-    // },
   },
   fontFamily: {
     title: ["Inter", "sans-serif"],
     body: ["Roboto", "sans-serif"],
   },
   variants: {
-    extend: {},
+    extend: {
+      fill: ["hover", "focus"],
+    },
   },
   plugins: [],
 };
