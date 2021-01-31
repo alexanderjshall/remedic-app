88484eebb75a450cc168d3aada85601d2527db85
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-01-31 14:03:34 +0100

    style(FE):added global styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import "./App.css";
+import LanguageChoice from "./Components/Pages/LanguageChoice/LanguageChoice";
 import Login from "./Components/Pages/Login/Login";
 
 function App() {
-  return (
-    <div className="App h-screen">
-      <Login />
-    </div>
-  );
+  return <div className="App h-screen flex justify-center"></div>;
 }
 
 export default App;
