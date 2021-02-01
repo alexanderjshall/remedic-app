aa7574d906253d4d4d4e1bb8b6cd6fdb29ba681e
Meera <meera.patel2984@gmail.com>
2021-02-01 10:56:14 +0000

    Feature: add doctor entity and doctor resolver.

--- server/src/migrations/Migration20210201090804.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20210201090804 extends Migration {
-
-  async up (): Promise<void> {
-    this.addSql('create table "patient" ("id" serial primary key);');
-  }
-
-}
