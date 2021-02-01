a6f99a7308b5dcb9a162e37cdf1638099e87be77
Alexander Jaye <alexanderjaye@outlook.com>
2021-02-01 08:11:27 +0000

    feat: Add success tick animation as global component

---
+++ client/src/Components/Globals/SuccessTick/SuccessTick.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Lottie, { Options } from 'react-lottie';
+import successTickAnimation from './33337-tick-pop.json'
+
+interface PropsSuccessTick {
+  size: number; // constricted ratio -> size is both width and height
+}
+
+const SuccessTick = (props: PropsSuccessTick) => {
+
+  // required Lottie Options for animation
+  const successTickOptions: Options = {
+    loop: false,
+    autoplay: true,
+    animationData: successTickAnimation,
+    rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
+  return (
+    <div>
+      <Lottie
+        options={successTickOptions}
+        height={props.size}
+        width={props.size}
+      ></Lottie>
+    </div>
+  )
+}
+
+export default SuccessTick
