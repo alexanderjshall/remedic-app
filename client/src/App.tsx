e453b63bc05923901e2692f8b4ab10899277b242
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-02-01 15:19:20 +0100

    add(FE): route for doctor chat

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
+import DoctorChat from "./Components/Pages/DoctorChat/DoctorChat";
 import LanguageChoice from "./Components/Pages/LanguageChoice/LanguageChoice";
 import Login from "./Components/Pages/Login/Login";
 import Register from "./Components/Pages/Register/Register";
@@ -18,6 +19,9 @@ function App() {
           </Route>
           <Route path="/language">
             <LanguageChoice />
+          </Route>
+          <Route path="/doctor_chat">
+            <DoctorChat />
           </Route>
         </Switch>
       </BrowserRouter>
