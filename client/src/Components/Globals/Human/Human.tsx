f3df748f3d3d02e637a501f7e37c3fe9b00391ab
Guillem <guillemgarciagomez@gmail.com>
2021-02-03 11:11:08 +0100

    Fix: set height of the component to 100% to make it scalable

--- client/src/Components/Globals/Human/Human.tsx
+++ client/src/Components/Globals/Human/Human.tsx
@@ -13,7 +13,7 @@ const Human = (props: Props) => {
   return (
   <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
      width="840.000000pt" height="1060.000000pt" viewBox="0 0 840.000000 1060.000000"
-    preserveAspectRatio="xMidYMid meet">
+    preserveAspectRatio="xMidYMid meet" className="h-full">
     <g transform="translate(0.000000,1060.000000) scale(0.100000,-0.100000)"
     stroke="none">
       {/* HEAD  */}
