d557f352cc44e5877e54854f20e071a696104be0
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-02-03 15:36:51 +0100

    add(FE): helper functions for auth context

--- client/src/Contexts/Auth.context.tsx
+++ client/src/Contexts/Auth.context.tsx
@@ -1,25 +1,41 @@
-import React, { createContext, useState, ReactChild } from 'react';
-import { User, blankUser } from '../types';
+import { queries } from "@testing-library/react";
+import React, { createContext, useState, ReactChild, useContext } from "react";
+import {
+  getTokenFromStorage,
+  checkUser,
+  AuthUser,
+} from "../utils/auth/auth.helper";
 
 export interface AppContextInterface {
-  user: User;
+  user: AuthUser | null;
 }
 
-export const AuthContext = createContext<AppContextInterface | null>(null);
+export const AuthContext = createContext<AppContextInterface>({ user: null });
 
 interface Props {
-  children: ReactChild | ReactChild[]
+  children: ReactChild | ReactChild[];
 }
 
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
 
 const AuthContextProvider = (props: Props) => {
-  const [user, setUser] = useState<User>(blankUser);
+  const [user, setUser] = useState<AuthUser | null>(null);
+
+  async function setUpContext() {
+    const savedUser = getTokenFromStorage();
+    const checkedUser = await checkUser(savedUser);
+    setUser(checkedUser);
+  }
+
+  setUpContext();
 
   return (
     <AuthContext.Provider value={{ user }}>
       {props.children}
     </AuthContext.Provider>
   );
-}
+};
 
 export default AuthContextProvider;
