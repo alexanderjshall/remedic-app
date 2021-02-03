ceeb174e7e3b25b7236426290f00d79870e50400
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-02-03 12:40:55 +0100

    cleanup(FE): imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,4 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import "./App.css";
-import DoctorChat from "./Components/Pages/DoctorChat/DoctorChat";
-import ConsultationChat from "./Components/Pages/ConsultationChat/ConsultationChat";
-import EnterCode from "./Components/Pages/EnterCode/EnterCode";
-import Feedback from "./Components/Pages/Feedback/Feedback";
-import LanguageChoice from "./Components/Pages/LanguageChoice/LanguageChoice";
-import Login from "./Components/Pages/Login/Login";
-import PatientLanding from "./Components/Pages/PatientLanding/PatientLanding";
-import Register from "./Components/Pages/Register/Register";
-import PatientQueue from "./Components/Pages/PatientQueue/PatientQueue";
 import UnAuthApp from "./Components/Globals/Apps/UnAuthApp";
 import DoctorApp from "./Components/Globals/Apps/DoctorApp";
 import PatientApp from "./Components/Globals/Apps/PatientApp";
