a71253870b823a72edd4ec36ca555bc1a0b93add
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-02-03 17:37:09 +0100

    add(BE): cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import { apolloServer } from './graphql';
@@ -11,7 +12,7 @@ const port = process.env.PORT;
 const chatPort = process.env.CHAT_PORT;
 
 const app = express();
-app.use(cookieParser());
+app.use(cookieParser(), cors());
 
 async function main () {
 
