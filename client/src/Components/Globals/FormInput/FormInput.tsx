7ac208cc7a5a44ae154292160d1b2eb01caf6862
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-02-04 11:14:27 +0100

    style(FE): styled global formn

--- client/src/Components/Globals/FormInput/FormInput.tsx
+++ client/src/Components/Globals/FormInput/FormInput.tsx
@@ -37,7 +37,7 @@ function FormInput(props: Props) {
       name={name}
       onChange={handleInputChange}
       onKeyPress={handleEnter}
-      className="px-4 mt-3 rounded-md shadow-sm p-2 ring-2 focus:ring-blue-dark w-full tablet:w-2/3"
+      className="px-4 rounded-md shadow-sm p-2 ring-2 focus:ring-blue-dark w-full tablet:w-2/3"
     />
   );
 }
