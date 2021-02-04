a5f4b785cdb6dd4baae182fab687feffbfa888d9
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-02-04 10:22:09 +0100

    Style(FE): - add logo - Madelanguage choice scrollable - removed flags

--- client/src/Components/Pages/LanguageChoice/LanguageChoice.tsx
+++ client/src/Components/Pages/LanguageChoice/LanguageChoice.tsx
@@ -1,26 +1,45 @@
 import React from "react";
 import { IoLanguageSharp } from "react-icons/io5";
-import "./LanguageChoice.css";
+import RemedicLogo from "../../../assets/logos/Remedic Text Logo.png";
 import OKButton from "../../Globals/OKButton/OKButton";
+import supportedLanguages from "../../../utils/supported-languages.json";
 
 const LanguageChoice = () => {
   const handleSubmit = () => {};
 
   return (
     <div className="flex-col flex justify-center w-screen">
-      <div className="flex flex-col shadow-md px-16 h-screen rounded-lg justify-evenly items-center">
+      <div className="flex flex-col shadow-md px-16 h-screen rounded-lg justify-evenly items-center overflow-hidden">
         <div>
           {" "}
-          <h1 className="text-3xl underline">Remedic</h1>
+          {/* <h1 className="text-3xl underline">Remedic</h1> */}
+          <div>
+            <img src={RemedicLogo} alt="Remedic Logo" />
+          </div>
         </div>
         <div className="">
-          <IoLanguageSharp className="inline mr-3 h-5 w-5" />
-          <p className=" text-lg inline">Please choose your language:</p>
-          <form
-            action=""
-            className="mt-5 flex flex-col divide-y-2 divide-black divide-opacity-30"
-          >
-            <div className="flex align-center justify-between my-2">
+          <div className="flex flex-col items-center mb-4">
+            <IoLanguageSharp className="inline mr-3 h-5 w-5 self-center" />
+            <p className=" text-lg inline whitespace-nowrap">
+              Please choose your language:
+            </p>
+          </div>
+          <form className="mt-5 flex flex-col divide-y-2 divide-black divide-opacity-30 p-3 max-h-96 overflow-y-auto">
+            {supportedLanguages &&
+              supportedLanguages.languages.map((language) => (
+                <div className="flex align-center justify-between my-2">
+                  <label htmlFor={language.langCode} className="text-lg">
+                    {language.nativeName}
+                  </label>
+                  <input
+                    type="radio"
+                    name="lang"
+                    id={language.langCode}
+                    value={language.englishName}
+                  />
+                </div>
+              ))}
+            {/* <div className="flex align-center justify-between my-2">
               <label htmlFor="hindi" className="text-lg">
                 हिन्दी 🇮🇳
               </label>
@@ -70,7 +89,7 @@ const LanguageChoice = () => {
                 普通话 🇨🇳
               </label>
               <input type="radio" name="lang" id="Mandarin" value="Mandarin" />
-            </div>
+            </div> */}
           </form>
         </div>
         <OKButton
