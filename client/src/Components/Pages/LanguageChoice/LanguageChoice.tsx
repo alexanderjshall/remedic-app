fb858aca60b0f0c4b59bb4820b845960793d89a2
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-02-04 10:24:11 +0100

    cleanup(FE): commented code

--- client/src/Components/Pages/LanguageChoice/LanguageChoice.tsx
+++ client/src/Components/Pages/LanguageChoice/LanguageChoice.tsx
@@ -12,7 +12,6 @@ const LanguageChoice = () => {
       <div className="flex flex-col shadow-md px-16 h-screen rounded-lg justify-evenly items-center overflow-hidden">
         <div>
           {" "}
-          {/* <h1 className="text-3xl underline">Remedic</h1> */}
           <div>
             <img src={RemedicLogo} alt="Remedic Logo" />
           </div>
@@ -39,57 +38,6 @@ const LanguageChoice = () => {
                   />
                 </div>
               ))}
-            {/* <div className="flex align-center justify-between my-2">
-              <label htmlFor="hindi" className="text-lg">
-                हिन्दी 🇮🇳
-              </label>
-              <input type="radio" name="lang" id="hindi" value="Hindi " />
-            </div>
-
-            <div className="flex align-center justify-between my-2">
-              <label htmlFor="Pakistani" className="text-lg">
-                اردو 🇵🇰
-              </label>
-              <input
-                type="radio"
-                name="lang"
-                id="Pakistani"
-                value="Pakistani"
-              />
-            </div>
-
-            <div className="flex align-center justify-between my-2">
-              <label htmlFor="Spanish" className="text-lg">
-                Español 🇪🇸
-              </label>
-              <input type="radio" name="lang" id="Spanish" value="Spanish" />
-            </div>
-
-            <div className="flex align-center justify-between my-2">
-              <label htmlFor="Vietnamese" className="text-lg">
-                Tiếng Việt 🇻🇳
-              </label>
-              <input
-                type="radio"
-                name="lang"
-                id="Vietnamese"
-                value="Vietnamese"
-              />
-            </div>
-
-            <div className="flex align-center justify-between my-2">
-              <label htmlFor="Russian" className="text-lg">
-                русский 🇷🇺
-              </label>
-              <input type="radio" name="lang" id="Russian" value="Russian" />
-            </div>
-
-            <div className="flex align-center justify-between my-2">
-              <label htmlFor="Mandarin" className="text-lg">
-                普通话 🇨🇳
-              </label>
-              <input type="radio" name="lang" id="Mandarin" value="Mandarin" />
-            </div> */}
           </form>
         </div>
         <OKButton
