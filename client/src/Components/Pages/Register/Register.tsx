12e4a6be16ecd8b819e8bb5dbc659e12accbe9c3
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-02-06 13:10:42 +0100

    style(FE): register form

--- client/src/Components/Pages/Register/Register.tsx
+++ client/src/Components/Pages/Register/Register.tsx
@@ -11,7 +11,6 @@ import { validateSignupForm } from "../../../utils/auth/validation.helper";
 import humanStanding from "../../../assets/background-images/humans-standing2.png";
 import logoReduced from "../../../assets/logos/logo-reduced.svg";
 
-
 const Register = () => {
   const { registerPatient } = useAuth();
   const history = useHistory();
@@ -56,19 +55,18 @@ const Register = () => {
   };
 
   return (
-    <div className="flex justify-center bg-white-dark h-screen p-4">
+    <div className="flex justify-center flex-col bg-white-dark h-full px-4 py-8">
+      <div className="mb-4 z-10">
+        <h2 className="bg-gradient-to-r from-green-light to-blue-light bg-clip-text text-transparent text-5xl font-bold px-6 py-5 text-center">
+          Register
+        </h2>
+      </div>
       <form
-        className="relative bg-white flex-col h-full shadow-lg rounded-lg w-full space-y-5 p-4 pt-8 grid place-items-center grid-rows-6 gap-y-10"
+        className="relative bg-white flex-col shadow-lg rounded-lg w-full p-4 grid place-items-center gap-y-6 tablet:h-full"
         onSubmit={handleSubmit}
       >
-        <div className="mb-4 z-10">
-          <h2 className="bg-gradient-to-r from-green-light to-blue-light bg-clip-text text-transparent text-5xl font-bold px-6 py-5">
-            Register
-          </h2>
-        </div>
-
-        <div className=" flex items-center justify-between flex-col w-3/4 tablet:w-2/3 pb-2 z-10">
-          <label htmlFor="firstName" className="mb-2 font-bold text-blue">
+        <div className="flex flex-col w-full items-center">
+          <label htmlFor="firstName" className=" font-bold text-blue">
             First Name:
           </label>
           <FormInput
@@ -81,8 +79,8 @@ const Register = () => {
           />
         </div>
 
-        <div className=" flex items-center justify-between flex-col w-3/4 tablet:w-2/3 pb-2 z-10">
-          <label htmlFor="lastName" className="mb-2 font-bold text-blue">
+        <div className="flex flex-col w-full items-center">
+          <label htmlFor="lastName" className="font-bold text-blue">
             Last Name:
           </label>
           <FormInput
@@ -94,8 +92,9 @@ const Register = () => {
             onSubmit={() => {}}
           />
         </div>
-        <div className=" flex items-center justify-items-center flex-col w-3/4 tablet:w-2/3 pb-2 z-10">
-          <label htmlFor="postCode" className="mb-2 font-bold text-blue">
+
+        <div className="flex flex-col w-full items-center">
+          <label htmlFor="postCode" className=" font-bold text-blue">
             Postcode:
           </label>
           <FormInput
@@ -107,8 +106,9 @@ const Register = () => {
             onSubmit={() => {}}
           />
         </div>
-        <div className=" flex items-center justify-items-center flex-col w-3/4 tablet:w-2/3 pb-2 z-10">
-          <label htmlFor="email" className="mb-2 font-bold text-blue">
+
+        <div className="flex flex-col w-full items-center">
+          <label htmlFor="email" className=" font-bold text-blue">
             Email Address:
           </label>
           <FormInput
@@ -120,8 +120,9 @@ const Register = () => {
             onSubmit={() => {}}
           />
         </div>
-        <div className="flex items-center justify-items-center flex-col w-3/4 tablet:w-2/3 pb-2 z-10">
-          <label htmlFor="password" className="mb-2 font-bold text-blue">
+
+        <div className="flex flex-col w-full items-center">
+          <label htmlFor="password" className=" font-bold text-blue">
             Password:
           </label>
           <FormInput
@@ -133,7 +134,7 @@ const Register = () => {
             onSubmit={() => {}}
           />
         </div>
-        <div className="flex flex-col align-center">
+        <div className="flex flex-col align-center mt-4">
           <AuthButton
             name="Register Button"
             value="Register"
@@ -170,7 +171,7 @@ const Register = () => {
       <img
         src={logoReduced}
         alt="background logo"
-        className="absolute w-24 top-5 opacity-10"
+        className="absolute w-24 top-5 opacity-10 left-1/2 transform-gpu -translate-x-1/2"
       ></img>
     </div>
   );
