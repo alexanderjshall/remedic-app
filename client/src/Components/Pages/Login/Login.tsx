a5482a9f8ce6354f1d83583ed5477ac65cb5687a
Meera <meera.patel2984@gmail.com>
2021-02-09 10:57:21 +0000

    Feature: add fade-in to the Language, Login, Patient Landing and Enter Code pages

--- client/src/Components/Pages/Login/Login.tsx
+++ client/src/Components/Pages/Login/Login.tsx
@@ -48,16 +48,12 @@ const Login = () => {
     <Transition
       appear={true}
       show={true}
-      enter="transform duration-1000"
-      // enterFrom="translate-x-0"
-      enterTo="translate-x-40"
-      // enter="transition-opacity duration-75"
-      // enterFrom="opacity-0"
-      // enterTo="opacity-100"
-
-
+      enter="transition-opacity ease-in-out duration-1000"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      className="flex items-center justify-center flex-col bg-white-dark min-h-screen lg:m-1 w-inherit min-w-min py-4"
     >
-    <div className="flex items-center justify-center flex-col bg-white-dark min-h-screen lg:m-1 w-inherit min-w-min py-4">
+    {/* <div className="flex items-center justify-center flex-col bg-white-dark min-h-screen lg:m-1 w-inherit min-w-min py-4"> */}
       <form
         className="relative bg-white h-full w-5/6 max-w-xl shadow-lg  rounded-lg p-12 flex flex-col items-center justify-between py-20"
         onSubmit={handleSubmit}
@@ -122,7 +118,7 @@ const Login = () => {
       >
         <p className="text-lg text-center">Invalid email or password</p>
       </div>
-    </div>
+    {/* </div> */}
     </Transition>
   );
 };
