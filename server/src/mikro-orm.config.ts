78c28956dada7b61812acf8bcce903205e90fba6
Baiju Joseph <baiju.joseph@gmail.com>
2021-02-09 12:11:32 +0000

    commit for deployment-test9

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -1,11 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import fs from 'fs';
 import { MikroORM } from '@mikro-orm/core';
 import path from 'path';
 import Patient from './entities/patient';
 import Doctor from './entities/doctor';
 import Consultation from './entities/consultation';
+
+const cert = fs.readFileSync(path.join( __dirname, '../server.crt')).toString();
 
 export default {
   migrations: {
@@ -18,4 +21,13 @@ export default {
   password: process.env.DB_PASS,
   type: 'postgresql',
   debug: process.env.NODE_ENV !== 'production',
+  driverOptions: {
+    connection: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true,
+        ca: cert
+      }
+    }
+  }
 } as Parameters<typeof MikroORM.init>[0];
