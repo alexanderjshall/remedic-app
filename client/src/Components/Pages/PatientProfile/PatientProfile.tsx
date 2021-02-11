28982917758b6b40e8fc5be0c6350a29db6acb2e
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-02-11 11:13:11 +0100

    style(FE): consistent headers

--- client/src/Components/Pages/PatientProfile/PatientProfile.tsx
+++ client/src/Components/Pages/PatientProfile/PatientProfile.tsx
@@ -1,26 +1,30 @@
-import React, { useContext, useRef, useState } from 'react'
+import React, { useContext, useRef, useState } from "react";
 import { useAuth } from "../../../Contexts/Auth.context";
 import { PatientContext } from "../../../Contexts/Patient.context";
 import { useHistory } from "react-router-dom";
-import OKButton from '../../Globals/OKButton/OKButton';
+import OKButton from "../../Globals/OKButton/OKButton";
 import FormInput from "../../Globals/FormInput/FormInput";
-import PatientProfileField from './PatientProfileField';
-import { UserData } from '../../../types';
-import humanStanding from '../../../assets/background-images/humans-sitting3.png';
-import defaultPicture from '../../../assets/profile-picture/default-profile-picture.png'
-import { Transition } from '@headlessui/react';
-
+import PatientProfileField from "./PatientProfileField";
+import { UserData } from "../../../types";
+import humanStanding from "../../../assets/background-images/humans-sitting3.png";
+import defaultPicture from "../../../assets/profile-picture/default-profile-picture.png";
+import { Transition } from "@headlessui/react";
 
 export interface ProfileField {
   name: string;
   value: string;
   type: string;
-  info: 'firstName'|'lastName'|'postCode'|'email' | 'language';
-  updateValue: (info: 'firstName'|'lastName'|'postCode'|'email'|'language', value: string) => void;
+  info: "firstName" | "lastName" | "postCode" | "email" | "language";
+  updateValue: (
+    info: "firstName" | "lastName" | "postCode" | "email" | "language",
+    value: string
+  ) => void;
 }
 
 function PatientProfile() {
-  const { getTranslatedText, patientInfo, updatePatient } = useContext(PatientContext)!;
+  const { getTranslatedText, patientInfo, updatePatient } = useContext(
+    PatientContext
+  )!;
   const [newPatientInfo, setNewPatientInfo] = useState<UserData>(patientInfo);
   const [currentPicture, setCurrentPicture] = useState<string>(defaultPicture);
   const [isPictureSaved, setIsPictureSaved] = useState<boolean>(true);
@@ -33,10 +37,13 @@ function PatientProfile() {
   const localTextUtils = translatedText.utils;
 
   const handleConfirmClick = () => {
-    history.push('/patient/landing');
-  }
+    history.push("/patient/landing");
+  };
 
-  const updateValue = (info: 'firstName'|'lastName'|'postCode'|'email'|'language', value: string) => {
+  const updateValue = (
+    info: "firstName" | "lastName" | "postCode" | "email" | "language",
+    value: string
+  ) => {
     let newPatient = Object.assign({}, newPatientInfo);
     newPatient[info] = value;
     setNewPatientInfo(newPatient);
@@ -47,47 +54,53 @@ function PatientProfile() {
     {
       name: localTextRegister.firstName,
       value: patientInfo.firstName,
-      type: 'text',
-      info: 'firstName',
-      updateValue
+      type: "text",
+      info: "firstName",
+      updateValue,
     },
     {
       name: localTextRegister.lastName,
       value: patientInfo.lastName,
-      type: 'text',
-      info: 'lastName',
-      updateValue
+      type: "text",
+      info: "lastName",
+      updateValue,
     },
     {
       name: localTextRegister.postcode,
       value: patientInfo.postCode,
-      type: 'text',
-      info: 'postCode',
-      updateValue
+      type: "text",
+      info: "postCode",
+      updateValue,
     },
     {
       name: localTextRegister.email,
       value: patientInfo.email,
-      type: 'email',
-      info: 'email',
-      updateValue
-    }
+      type: "email",
+      info: "email",
+      updateValue,
+    },
   ];
 
   const handleChangePicture = (files: FileList | null) => {
     if (files) {
       const newPicture = files[0];
-      if(!newPicture.type.startsWith('image/')){return}
+      if (!newPicture.type.startsWith("image/")) {
+        return;
+      }
       const reader = new FileReader();
       reader.onload = function (e) {
-        if (e.target && e.target.result && typeof(e.target.result) === 'string') {
+        if (
+          e.target &&
+          e.target.result &&
+          typeof e.target.result === "string"
+        ) {
           setCurrentPicture(e.target.result);
           setIsPictureSaved(false);
         }
-      }
+      };
       reader.readAsDataURL(newPicture);
     }
-  }
+  };
 
   const handleSavePicture = () => {
     setIsPictureSaved(true);
@@ -100,55 +113,68 @@ function PatientProfile() {
 
   return (
     <Transition
-    appear={true}
-    show={true}
-    enter="transition-opacity delay-75 ease-in-out duration-500"
-    enterFrom="opacity-0"
-    enterTo="opacity-100"
-    className="h-screen relative flex flex-col justify-start items-center w-screen overflow-hidden px-0"
-  >
+      appear={true}
+      show={true}
+      enter="transition-opacity delay-75 ease-in-out duration-500"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      className="h-screen relative flex flex-col justify-start items-center w-screen overflow-hidden px-0"
+    >
       <div className="h-5/6 m-1.5 px-0 py-4 z-10 flex flex-col items-center md:w-8/12">
-        <h1 className="text-center text-xl font-extrabold text-blue border border-solid border-black rounded-3xl py-2 px-1 min-w-full mb-5 shadow-lg">
+        <h1 className="text-center text-xl font-extrabold text-blue border border-solid border-gray-light py-2 px-1 min-w-full mb-5 shadow-lg rounded-md">
           {translatedText.patientLandingTerms.profile}
         </h1>
         <div className="flex items-center ">
-          <div className="shadow-xl rounded-xl border border-black border-3 border-opacity-20 p-1 maxHeight-1 max-h-18 mr-4">
-            <img src={currentPicture} alt="profile"
+          <div className="shadow-xl rounded-xl border border-gray-light border-3 border-opacity-20 p-1 maxHeight-1 max-h-18 mr-4">
+            <img
+              src={currentPicture}
+              alt="profile"
               className="block h-16 w-16 object-scale-down"
             />
           </div>
-          {isPictureSaved
-            ? <label className="border-invisible rounded-md px-4 py-1 ml-2 bg-blue-superlight cursor-pointer">
-                {localText.changePicture}
-                <input id="pictureUpload" type="file" ref={pictureInputRef}
+          {isPictureSaved ? (
+            <label className="border-invisible rounded-md px-4 py-1 ml-2 bg-blue-superlight cursor-pointer">
+              {localText.changePicture}
+              <input
+                id="pictureUpload"
+                type="file"
+                ref={pictureInputRef}
                 className="hidden"
-                onChange={(e) => {handleChangePicture(e.currentTarget.files)}}
-                />
-              </label>
-            : <div>
-              <button className="border-invisible rounded-md px-4 py-1 bg-green-dark text-white ml-1"
-                      onClick={() => handleSavePicture()}
-                      type='submit'
-              >{localText.save}</button>
-              <button className="border-invisible rounded-md px-4 py-1 bg-red-negative text-white ml-2"
-                      onClick={() => handleCancelPicture()}
-              >{localText.cancel}</button>
+                onChange={(e) => {
+                  handleChangePicture(e.currentTarget.files);
+                }}
+              />
+            </label>
+          ) : (
+            <div>
+              <button
+                className="border-invisible rounded-md px-4 py-1 bg-green-dark text-white ml-1"
+                onClick={() => handleSavePicture()}
+                type="submit"
+              >
+                {localText.save}
+              </button>
+              <button
+                className="border-invisible rounded-md px-4 py-1 bg-red-negative text-white ml-2"
+                onClick={() => handleCancelPicture()}
+              >
+                {localText.cancel}
+              </button>
             </div>
-          }
+          )}
         </div>
         <div>
           {profileFields.map((profile, i) => (
-              <div key={i} className="mx-1">
-                <PatientProfileField
-                  name={profile.name}
-                  value={profile.value}
-                  type={profile.type}
-                  info={profile.info}
-                  updateValue={updateValue}
-                />
-              </div>
-          )
-          )}
+            <div key={i} className="mx-1">
+              <PatientProfileField
+                name={profile.name}
+                value={profile.value}
+                type={profile.type}
+                info={profile.info}
+                updateValue={updateValue}
+              />
+            </div>
+          ))}
         </div>
       </div>
 
@@ -160,12 +186,12 @@ function PatientProfile() {
         onClick={handleConfirmClick}
       />
       <img
-          src={humanStanding}
-          alt="background human"
-          className="absolute opacity-10 top-1/2 left-10 -mt-56 w-5/6 max-w-3xl transform -scale-x-1"
-        ></img>
+        src={humanStanding}
+        alt="background human"
+        className="absolute opacity-10 top-1/2 left-10 -mt-56 w-5/6 max-w-3xl transform -scale-x-1"
+      ></img>
     </Transition>
-  )
+  );
 }
 
-export default PatientProfile
+export default PatientProfile;
--- client/src/Components/Pages/PatientProfile/PatientProfileField.tsx
+++ client/src/Components/Pages/PatientProfile/PatientProfileField.tsx
@@ -1,10 +1,10 @@
-import React, { useContext, useEffect, useState } from 'react'
-import {ProfileField} from './PatientProfile';
+import React, { useContext, useEffect, useState } from "react";
+import { ProfileField } from "./PatientProfile";
 import { PatientContext } from "../../../Contexts/Patient.context";
 
 function PatientProfileField(props: ProfileField) {
   const [isEditing, setIsEditing] = useState<boolean>(false);
-  const {name, value, type, info, updateValue} = props;
+  const { name, value, type, info, updateValue } = props;
   const [newValue, setNewValue] = useState<string>(value);
 
   const { getTranslatedText } = useContext(PatientContext)!;
@@ -13,57 +13,76 @@ function PatientProfileField(props: ProfileField) {
 
   useEffect(() => {
     setNewValue(value);
-  }, [])
+  }, []);
 
   const onEditClick = () => {
     setIsEditing(true);
     setNewValue(value);
-  }
+  };
 
   const handleSubmit = () => {
     updateValue(info, newValue);
     setIsEditing(false);
-  }
+  };
 
   const handleEnter: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
-    if (e.key === 'Enter') handleSubmit();
-  }
+    if (e.key === "Enter") handleSubmit();
+  };
 
   const onCancelClick = () => {
     setNewValue(value);
     setIsEditing(false);
-  }
+  };
 
   const handleValueChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     e.preventDefault();
     setNewValue(e.currentTarget.value);
-  }
+  };
 
   return (
-    <div className="mt-3 w-full border border-1 border-solid border-black rounded-md p-2">
+    <div className="mt-3 w-full rounded-xl shadow-lg bg-gray-100 p-2">
       <h3 className="mb-1">{name}</h3>
-        {isEditing
-          ? <div className="flex justify-between items-center flex-wrap m-1.5">
-                <input type="text" id={info} value={newValue} onChange={handleValueChange} autoFocus={isEditing}
-                className="px-2 rounded-md shadow-sm py-1 ring-2 focus:ring-blue-dark w-1/2 cursor-text"
-                onSubmit={()=>handleSubmit()} onKeyPress={handleEnter}/>
-                <button className="border-invisible rounded-md px-4 py-1 bg-green-dark text-white ml-1"
-                        onClick={() => handleSubmit()}
-                        type='submit'
-                >{localText.save}</button>
-                <button className="border-invisible rounded-md px-4 py-1 bg-red-negative text-white ml-1" onClick={() => onCancelClick()}
-                >{localText.cancel}</button>
-
-            </div>
-          :  <div className="flex justify-between items-center flex-wrap">
-              <p><b>{value}</b></p>
-              <button className="border-invisible rounded-md px-4 py-1 ml-2 bg-blue-superlight" onClick={() => onEditClick()}
-              >{localText.edit}</button>
-            </div>
-
-        }
+      {isEditing ? (
+        <div className="flex justify-between items-center flex-wrap m-1.5">
+          <input
+            type="text"
+            id={info}
+            value={newValue}
+            onChange={handleValueChange}
+            autoFocus={isEditing}
+            className="px-2 rounded-md shadow-sm py-1 ring-2 focus:ring-blue-dark w-1/2 cursor-text"
+            onSubmit={() => handleSubmit()}
+            onKeyPress={handleEnter}
+          />
+          <button
+            className="border-invisible rounded-md px-4 py-1 bg-green-dark text-white ml-1"
+            onClick={() => handleSubmit()}
+            type="submit"
+          >
+            {localText.save}
+          </button>
+          <button
+            className="border-invisible rounded-md px-4 py-1 bg-red-negative text-white ml-1"
+            onClick={() => onCancelClick()}
+          >
+            {localText.cancel}
+          </button>
+        </div>
+      ) : (
+        <div className="flex justify-between items-center flex-wrap">
+          <p>
+            <b>{value}</b>
+          </p>
+          <button
+            className="border-invisible rounded-md px-4 py-1 ml-2 bg-blue-superlight"
+            onClick={() => onEditClick()}
+          >
+            {localText.edit}
+          </button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default PatientProfileField;
